Fixed output scaling

products.py
@@ -2,71 +2,45 @@ import os
 
 import numpy as np
 import pyfits
-from scipy.interpolate import RectBivariateSpline
 
-from util import sf
+from util import sf, resample2d
 
 class cube():
   def __init__(self, logger, shape):
     self.data = np.zeros(shape=shape)
     self.logger = logger
-    self.params = []
     self.cube_idx = 0
   
   def addComposite(self, composite):
     self.data[self.cube_idx] 	= composite.data
-    p = {}
-    p['WAVE'] 			= composite.wave
-    p['RESOLUTION_ELEMENT']	= composite.resolution_element
-    p['PSCALE'] 		= composite.pscale
-    p['DETECTOR_FOV']	 	= composite.detector_FOV
-    self.params.append(p)
     self.cube_idx += 1
   
-  def write(self, fname, resample, sampling_factor, fov):
-    data = []
-    header = pyfits.Header()
-    waves = []
-    for p in self.params:
-      waves.append(p['WAVE'])
-    sampling_post_rebin 	= self.params[np.argmin(waves)]['RESOLUTION_ELEMENT']/sampling_factor	# desired "/px
-    
-    header.append(('CRVAL1', -fov/2))
+  def write(self, fname, resampling_im, resampling_factor, hfov, verbose=False):
+    sampling_pre_rebin 		= resampling_im.pscale					# current "/px
+    hfov_pre_rebin		= resampling_im.getDetectorHFOV()
+    sampling_post_rebin 	= resampling_im.resolution_element/resampling_factor	# desired "/px
+    hfov_post_rebin		= hfov
+
+    header = pyfits.Header()    
+    header.append(('CRVAL1', -hfov))
     header.append(('CDELT1', sampling_post_rebin))
     header.append(('CRPIX1', 0.5))
     header.append(('CUNIT1', "arcsec"))
     header.append(('CTYPE1', "PARAM"))
-    header.append(('CRVAL2', -fov/2))
+    header.append(('CRVAL2', -hfov))
     header.append(('CDELT2', sampling_post_rebin))
     header.append(('CRPIX2', 0.5))
     header.append(('CUNIT2', "arcsec"))
     header.append(('CTYPE2', "PARAM"))  
     
-    for d, p in zip(self.data, self.params):
-      sampling_pre_rebin 	= p['PSCALE']								# current "/px
-      hfov_pre_rebin		= p['DETECTOR_FOV']/2
-      hfov_post_rebin		= fov/2
-      
-      # set up original grid
-      grid_pre_rebin = np.arange(-hfov_pre_rebin, hfov_pre_rebin, sampling_pre_rebin)
-      G = RectBivariateSpline(grid_pre_rebin, grid_pre_rebin, d, kx=3, ky=3)
-      
-      if resample=="shortest":
-        self.logger.debug(" Resampling " + str(sf(p['WAVE']*10**9, 3)) + "nm to shortest wavelength (" + str(sf(np.min(waves)*10**9, 3)) + "nm)...")
-        # evaluate at new grid sampling, taking into account new fov
-        grid_x, grid_y = np.mgrid[-hfov_post_rebin:hfov_post_rebin:sampling_post_rebin, -hfov_post_rebin:hfov_post_rebin:sampling_post_rebin]
-        H = G.ev(grid_x, grid_y)
-      else:
-        self.logger.debug(" Not resampling data (are you sure you want this?)")
-        # evaluate at old grid sampling, taking into account new fov
-        grid_x, grid_y = np.mgrid[-hfov_post_rebin:hfov_post_rebin:sampling_pre_rebin, -hfov_post_rebin:hfov_post_rebin:sampling_pre_rebin]
-        H = G.ev(grid_x, grid_y)	
-      data.append(H)
+    data = []
+    for d in self.data:
+      data.append(resample2d(d, -hfov_pre_rebin, hfov_pre_rebin, sampling_pre_rebin, -hfov_post_rebin, hfov_post_rebin, sampling_post_rebin))
     data = np.array(data)
     
     if os.path.exists(fname):
       os.remove(fname)
-    self.logger.debug(" Writing output... ")
+    self.logger.debug(" Writing output to " + str(fname) + ".")
     pyfits.writeto(fname, data, header)
     
   class composite():

pupil.py
@@ -4,21 +4,26 @@ from copy import deepcopy
 
 import numpy as np
 
-from util import sf
+from util import sf, resample2d
 
 class pupil(object):
-  def __init__(self, logger, camera, sampling, gamma, verbose):
+  def __init__(self, logger, camera, sampling, gamma, verbose, data=None):
     self.logger		 	= logger
     self.camera			= camera
     self.sampling 		= sampling					# sampling size for pupil (pixels)
-    self.gamma 			= gamma						# factor by which to pad the grid (==number of pixels per resolution element in image plane)
-    self.gsize			= self.sampling*self.gamma			# size of padded grid in pixels
-    self.physical_gsize		= None						# physical size of padded grid in units of [physical_gsize_unit]
-    self.pupil_plate_scale	= None						# [physical_gsize_unit/px]
-    self.physical_gsize_unit	= None						# physical unit of grid
-    self.data			= None
+    self.gamma 			= gamma						# factor by which to pad the grid (=px/resolution element)
+    self.physical_gsize		= None						# physical size of padded grid in units of mm
+    self.pupil_plate_scale	= None						# [mm/px]
     
     self.image_slicing_region	= None
+    
+    # set up a new data grid if data field is empty
+    if data is None:
+      self.gsize	= self.sampling*self.gamma			# size of padded grid in pixels
+      self._setup()
+    else:
+      self.gsize	= data.shape[0]
+      self.data		= data
     
   def _setup(self):
     mag = np.ones((self.gsize,self.gsize))
@@ -95,30 +100,20 @@ class pupil(object):
     pass
   
 class circular(pupil): 
-  def __init__(self, logger, camera, sampling, gamma, rad, physical_gsize_unit, verbose=False):
-    super(circular, self).__init__(logger, camera, sampling, gamma, verbose)
+  def __init__(self, logger, camera, sampling, gamma, rad, verbose=False, data=None):
+    super(circular, self).__init__(logger, camera, sampling, gamma, verbose, data)
     self.rad 					= rad
-    self.physical_gsize_unit 			= physical_gsize_unit
-    self.physical_entrance_diameter		= self.rad*2					# physical diameter of entrance pupil in units of [physical_gsize_unit]
+    self.physical_entrance_diameter		= self.rad*2					# physical diameter of entrance pupil in units of [mm]
     self.physical_gsize 			= self.physical_entrance_diameter*self.gamma	
-    self.pupil_plate_scale			= self.physical_entrance_diameter/sampling
+    self.pupil_plate_scale			= self.physical_gsize/self.gsize
 
-    # translate unit to numerical quantity so output scale is physically meaningful
-    if self.physical_gsize_unit == "m":
-      self.physical_gsize_mfactor = 1
-    elif self.physical_gsize_unit == "mm":
-      self.physical_gsize_mfactor = 1e-3
-    else:
-      self.logger.warning(" Unrecognised pupil radius unit, assuming mm.")
-      self.physical_gsize_mfactor = 1e-3
+    # translate unit to numerical quantity so output scales are physically meaningful
+    self.physical_gsize_mfactor = 1e-3
 
     if verbose:
-      self.logger.debug(" Entrance pupil diameter is " + str(self.physical_entrance_diameter) + self.physical_gsize_unit)
+      self.logger.debug(" Entrance pupil diameter is " + str(self.physical_entrance_diameter) + 'mm')
       self.logger.debug(" With pupil sampling of " + str(self.sampling) + "x" + str(self.sampling) + ", this corresponds to a pupil plate scale of " + 
-			sf(self.pupil_plate_scale, 2) + self.physical_gsize_unit + "/px.")
-      
-    # initialise data
-    self._setup() 
+			sf(self.pupil_plate_scale, 2) + "mm/px.")
    
     # this is just to maintain conformity with derived slicing class
     self.region					= (0, self.data.shape[0])
@@ -155,7 +150,7 @@ class circular(pupil):
       if verbose:
 	self.pupil.logger.debug(" At a wavelength of " + str(self.wave*10**9) + "nm, a system with a focal ratio of " + str(self.pupil.camera.wfno) + " with a circular aperture would have the following properties:")
 	self.pupil.logger.debug(" -> " + sf(self.resolution_element, 4) + "\"" + " (" + sf(self.pupil.camera.s_resolution_element, 4) + "μm)" + " per resolution element λ/D")
-	self.pupil.logger.debug(" -> " + sf(self.pscale, 4) + "\"" + " (" + sf(self.pupil.camera.s_pscale, 4) + "μm)" + " per pixel with γ=" + str(self.pupil.gamma) + " pixels per resolution element")
+	self.pupil.logger.debug(" -> " + sf(self.pscale, 4) + "\"" + " (" + sf(self.pupil.camera.s_pscale, 4) + "μm)" + " per pixel, with γ=" + str(self.pupil.gamma) + " pixels per resolution element")
 	self.pupil.logger.debug(" -> a detector FoV of " + sf(self.detector_FOV, 4) + "\"" + " (" + sf(self.pupil.camera.s_detector_FOV, 6) + "μm)")
 	self.pupil.logger.debug(" -> an airy disk diameter of " + sf(self.airy_disk_d, 4) + "\"" + " (" + sf(self.pupil.camera.s_airy_disk_d, 4) + "μm)")
      
@@ -216,15 +211,15 @@ class circular(pupil):
 
       return self.getAmplitude(power, shift, normalise, scale)[(self.pupil.gsize/2)-(im_npix/2):(self.pupil.gsize/2)+(im_npix/2), (self.pupil.gsize/2)-(im_npix/2):(self.pupil.gsize/2)+(im_npix/2)], detector_HFOV_scaled
     
-    def sliceUp(self, width_el, offset=0, slice_number=1, verbose=False):
+    def sliceUp(self, width_el, gamma, offset=0, slice_number=1, verbose=False):
       '''
-	Takes a slice of width [width_el] resolution element, offset by [offset] resolution elements from the centre
-	and pads the rest of the array with zeros.
+	Takes a slice of width [width_el] resolution elements (taken at the resample_to gamma to ensure a uniform
+	slice width), offset by [offset] resolution elements from the centre, and pads the rest of the array with zeros.
 	
 	This will permanently change the data and set the is_sliced flag.
       '''
-      half_slice_width 	= int(self.pupil.gamma*width_el)		# in pixels
-      offset_res 	= int(self.pupil.gamma*offset)			# in pixels
+      half_slice_width 	= int(gamma*width_el)		# in pixels
+      offset_res 	= int(gamma*offset)		# in pixels
       
       self.slice_number = slice_number
       self.slice_region	= ((self.pupil.gsize/2)-(half_slice_width/2)+offset_res, (self.pupil.gsize/2)+(half_slice_width/2)+offset_res)
@@ -247,24 +242,64 @@ class circular(pupil):
         Returns a new instance of either circular or circular_pupil, depending on whether
         the is_sliced flag has been set.
       '''
+      if ishift:
+	self.data = np.fft.ifftshift(self.data)   
+      self.data = np.fft.ifft2(self.data)
       if self.is_sliced:
         new_pupil = circular_slice(self.pupil.logger, self.pupil.camera, self.pupil.sampling, self.pupil.gamma, 
-				    self.pupil.rad, self.pupil.physical_gsize_unit, self.slice_number, self.slice_region, verbose)
+				    self.pupil.rad, self.slice_number, self.slice_region, verbose, self.data)
       else:
-	new_pupil = circular(self.pupil.logger, self.pupil.camera, self.pupil.sampling, self.pupil.gamma, self.pupil.rad, self.pupil.physical_gsize_unit, verbose)
-      new_pupil.data = deepcopy(self.data)
-      if ishift:
-	new_pupil.data = np.fft.ifftshift(new_pupil.data)   
-      new_pupil.data = np.fft.ifft2(new_pupil.data)
+	new_pupil = circular(self.pupil.logger, self.pupil.camera, self.pupil.sampling, self.pupil.gamma, self.pupil.rad, verbose, self.data)
+
       return new_pupil
     
+    def resample(self, new_pscale, new_hfov, verbose=False):
+      '''
+          Resample data to new pixel scale and fov.
+      '''
+      if np.isclose(self.pscale, new_pscale) and np.isclose(self.getDetectorHFOV(), new_hfov):
+	self.pupil.logger.debug(" Resampling not required for this wavelength.")
+      else:
+	self.pupil.logger.debug(" Data needs to be resampled from " + sf(self.pscale, 4) + " to " + sf(new_pscale, 4) + "\" per pixel.")
+	if np.issubdtype(self.data.dtype, np.complexfloating):
+	  self.pupil.logger.debug(" Resampling complex data requires discarding the imaginary part. This only makes sense if the phase is zero.")
+	  if np.allclose(np.imag(self.data), 0):
+	    self.pupil.logger.debug(" Checking to see if all imaginary parts are zero... OK")
+	  else:
+	    self.pupil.logger.critical(" Checking to see if all imaginary parts are zero... FAIL")
+	    exit(0)
+	  self.data = np.real(self.data)
+	
+	# resample
+	self.data = resample2d(self.data, -self.getDetectorHFOV(), self.getDetectorHFOV(), self.pscale, -new_hfov, new_hfov, new_pscale)
+        
+        # convert back to complex
+        self.data = self.data.astype(dtype=complex)
+        
+        # change some pupil parameters now we've rescaled
+        self.pupil.gamma 	= self.resolution_element/new_pscale			# note this will now no longer be an integer
+        
+        # change some image parameters now we've rescaled
+        self.pscale 		= self.resolution_element/self.pupil.gamma
+        self.detector_FOV 	= self.pscale*self.pupil.gsize	
+        
+        # recalculate camera properties
+        self.pupil.camera.populateSpatialParameters(self)
+        
+        if verbose:
+	  self.pupil.logger.debug(" Resampled data now has the following properties: ")
+	  self.pupil.logger.debug(" -> " + sf(self.resolution_element, 4) + "\"" + " (" + sf(self.pupil.camera.s_resolution_element, 4) + "μm)" + " per resolution element λ/D")
+	  self.pupil.logger.debug(" -> " + sf(self.pscale, 4) + "\"" + " (" + sf(self.pupil.camera.s_pscale, 4) + "μm)" + " per pixel, with γ=" + str(self.pupil.gamma) + " pixels per resolution element")
+	  self.pupil.logger.debug(" -> a detector FoV of " + sf(self.detector_FOV, 4) + "\"" + " (" + sf(self.pupil.camera.s_detector_FOV, 6) + "μm)")
+	  self.pupil.logger.debug(" -> an airy disk diameter of " + sf(self.airy_disk_d, 4) + "\"" + " (" + sf(self.pupil.camera.s_airy_disk_d, 4) + "μm)")
+	  
 class circular_slice(circular):
   '''
     This class has two extra fields on init compared with its parent, with details
     pertaining to the slice taken.
   '''
-  def __init__(self, logger, camera, sampling, gamma, rad, physical_gsize_unit, number, region, verbose):
-    super(circular_slice, self).__init__(logger, camera, sampling, gamma, rad, physical_gsize_unit, verbose)
+  def __init__(self, logger, camera, sampling, gamma, rad, number, region, verbose, data):
+    super(circular_slice, self).__init__(logger, camera, sampling, gamma, rad, verbose, data)
     self.number		= number
     self.region 	= region
     

sim.py
@@ -26,21 +26,22 @@ def isPowerOfTwo(num):
   return num == 1
 
 class sim():
-  def __init__(self, logger, plotter, nwaves, CAMERA_FWNO, PUPIL_SAMPLING, 
-	       PUPIL_GAMMA, PUPIL_RADIUS, PUPIL_RADIUS_UNIT, ADD_WFE, DO_SLICING, 
-	       NSLICES, SLICE_WIDTH):
+  def __init__(self, logger, plotter, resampling_im, nwaves, CAMERA_FWNO, PUPIL_SAMPLING, 
+	       PUPIL_GAMMA, PUPIL_RADIUS, ADD_WFE, DO_SLICING, 
+	       NSLICES, SLICE_WIDTH, RESAMPLE_TO):
     self.logger 		= logger
     self.plotter 		= plotter
+    self.resampling_im		= resampling_im
     self.nwaves			= nwaves
     self.CAMERA_FWNO		= CAMERA_FWNO
     self.PUPIL_SAMPLING 	= PUPIL_SAMPLING
     self.PUPIL_GAMMA		= PUPIL_GAMMA
     self.PUPIL_RADIUS		= PUPIL_RADIUS
-    self.PUPIL_RADIUS_UNIT	= PUPIL_RADIUS_UNIT
     self.ADD_WFE		= ADD_WFE
     self.DO_SLICING		= DO_SLICING
     self.NSLICES		= NSLICES
     self.SLICE_WIDTH		= SLICE_WIDTH
+    self.RESAMPLE_TO		= RESAMPLE_TO
     
     self.datacube		= cube(self.logger, shape=[self.nwaves, self.PUPIL_SAMPLING*self.PUPIL_GAMMA, self.PUPIL_SAMPLING*self.PUPIL_GAMMA])
    
@@ -48,6 +49,7 @@ class sim():
     """
       Run the simulation.
     """
+    # do a few sanity checks first
     try:
       self.PUPIL_SAMPLING 	= int(self.PUPIL_SAMPLING)
       self.PUPIL_GAMMA 		= int(self.PUPIL_GAMMA)
@@ -74,7 +76,7 @@ class sim():
       
     # instantiate camera, entrance pupil and output
     cam = camera(self.CAMERA_FWNO)
-    pupil = circular(self.logger, cam, self.PUPIL_SAMPLING, self.PUPIL_GAMMA, self.PUPIL_RADIUS, self.PUPIL_RADIUS_UNIT, verbose=verbose)  
+    pupil = circular(self.logger, cam, self.PUPIL_SAMPLING, self.PUPIL_GAMMA, self.PUPIL_RADIUS, verbose=verbose)  
     this_composite_image = self.datacube.composite(self.datacube, wave, pupil)
       
     # get WFE if requested
@@ -87,13 +89,15 @@ class sim():
       if wfe_h['SAMPLING'][0] != self.PUPIL_SAMPLING:
 	self.logger.critical(" Zemax WFE sampling is not the same as the pupil sampling! (" + str(wfe_h['SAMPLING'][0]) + " != " + str(self.PUPIL_SAMPLING) + ")")
 	exit(0)
-      pl.addImagePlot("wfe (radians)", np.abs(np.fft.fftshift(wfe_d)), extent=pupil.getExtent(), xl=self.PUPIL_RADIUS_UNIT, yl=self.PUPIL_RADIUS_UNIT)  
+      pl.addImagePlot("wfe (radians)", np.abs(np.fft.fftshift(wfe_d)), extent=pupil.getExtent(), xl='mm', yl='mm')  
 
-    # plot image of whole pupil unsliced (when slicing, this image is just for plotting purposes)
+    # rescale image to same plate scale as resampled_im
     im = pupil.toConjugateImage(wave, verbose=True)
     d, hfov = im.getAmplitudeScaledByAiryDiameters(3, normalise=True)
     pl.addImagePlot("-> fft to image space", d, extent=(-hfov, hfov, -hfov, hfov), xl="arcsec", yl="arcsec")
-
+    im.resample(self.resampling_im.pscale, self.resampling_im.getDetectorHFOV(), verbose=True)
+    pupil = im.toConjugatePupil(verbose=True)
+    
     # slicing
     if self.DO_SLICING:
       # take slices from the image space
@@ -101,7 +105,7 @@ class sim():
       for s in range(self.NSLICES):	
 	im = pupil.toConjugateImage(wave)				# SLICING SPACE CHANGE. move from pupil to image space. centered DC.
 	offset = (s-((self.NSLICES-1)/2))*self.SLICE_WIDTH
-	im.sliceUp(self.SLICE_WIDTH, offset=offset, slice_number=s+1, verbose=True)	# create a new pupil conjugate image instance for each slice
+	im.sliceUp(self.SLICE_WIDTH, offset=offset, gamma=self.resampling_im.pupil.gamma, slice_number=s+1, verbose=True)	# create a new pupil conjugate image instance for each slice
 	pl.addScatterPlot(None, [(-(self.SLICE_WIDTH*im.resolution_element)/2)+(offset*im.resolution_element), 
 				(-(self.SLICE_WIDTH*im.resolution_element)/2)+(offset*im.resolution_element)], [-hfov, hfov], xr=(-hfov, hfov), yr=(-hfov, hfov), overplot=True)
 	pl.addScatterPlot(None, [((self.SLICE_WIDTH*im.resolution_element)/2)+(offset*im.resolution_element), 
@@ -113,7 +117,7 @@ class sim():
 	 # fft back to pupil plane 
 	new_pupil = s.toConjugatePupil()				# SLICING SPACE CHANGE. move from image to pupil space. zeroed DC.
         pl.addImagePlot("-> take slice " + str(s.slice_number) + " -> ifft to pupil space", new_pupil.getAmplitude(shift=True, normalise=True), 
-			extent=new_pupil.getExtent(), xl=self.PUPIL_RADIUS_UNIT, yl=self.PUPIL_RADIUS_UNIT)
+			extent=new_pupil.getExtent(), xl='mm', yl='mm')
     	
         # add phase WFE 
         if self.ADD_WFE:
@@ -140,11 +144,11 @@ class sim():
 	  
       # move from pupil to image space	
       im = pupil.toConjugateImage(wave)  				# NON-SLICING SPACE CHANGE. move from pupil to image space. centered DC.
-	  
+      
       d, hfov = im.getAmplitudeScaledByAiryDiameters(3, normalise=True)
       pl.addImagePlot(plt_title_prefix + "-> fft to image space", d, extent=(-hfov, hfov, -hfov, hfov), xl="arcsec", yl="arcsec")
       
-      this_composite_image.add(im)
+      this_composite_image.add(im)					# take amplitude and add per slice
     
     if plot:
       pl.draw(5,5)
@@ -181,30 +185,36 @@ if __name__== "__main__":
   c.read("etc/default.ini")
   cfg = {}			# config parameters needed by __main__ *only*
   cfg_sim = {}			# config parameters needed by simulation instance
-  cfg['RESAMPLE']		= str(c.get("output", "resample"))
-  cfg['SAMPLING_FACTOR']	= int(c.get("output", "sampling_factor"))     
-  cfg['FOV']			= float(c.get("output", "fov"))    
+  cfg['RESAMPLING_FACTOR']	= int(c.get("output", "resampling_factor"))     
+  cfg['HFOV']			= float(c.get("output", "hfov"))    
   
   cfg_sim['CAMERA_FWNO']	= float(c.get("camera", "wfno"))
   
   cfg_sim['PUPIL_SAMPLING']	= float(c.get("pupil", "pupil_sampling"))
   cfg_sim['PUPIL_GAMMA'] 	= float(c.get("pupil", "pupil_gamma"))
   cfg_sim['PUPIL_RADIUS'] 	= float(c.get("pupil", "pupil_radius_physical"))
-  cfg_sim['PUPIL_RADIUS_UNIT']	= str(c.get("pupil", "pupil_radius_physical_unit"))
   
   cfg_sim['ADD_WFE']		= bool(int(c.get("wfe", "do")))
 
   cfg_sim['DO_SLICING']		= bool(int(c.get("slicing", "do")))
   cfg_sim['NSLICES']		= int(c.get("slicing", "number"))
   cfg_sim['SLICE_WIDTH']	= float(c.get("slicing", "width"))				# in resolution elements
+  cfg_sim['RESAMPLE_TO']	= float(c.get("slicing", "resample_to"))
 
   st = time.time()
   logger.debug(" Beginning simulation.")
   
   waves = np.arange(args.ws, args.we, args.wi)
-  s = sim(logger, plotter, len(waves), **cfg_sim)
+  
+  # first need to find the resampling parameters, we do this by create a conjugateImage instance
+  logger.debug(" Ascertaining parameters to resample to " + sf(cfg_sim['RESAMPLE_TO']*10**9,3) + "nm")
+  cam = camera(cfg_sim['CAMERA_FWNO'])
+  resampling_pupil = circular(logger, cam, cfg_sim['PUPIL_SAMPLING'], cfg_sim['PUPIL_GAMMA'], cfg_sim['PUPIL_RADIUS'], verbose=True) 
+  resampling_im = resampling_pupil.toConjugateImage(cfg_sim['RESAMPLE_TO'], verbose=True)
+  
+  s = sim(logger, plotter, resampling_im, len(waves), **cfg_sim)  
   for w in waves:
-    logger.info(" Processing for a wavelength of " + str(w*1e9) + "nm...")   
+    logger.info(" !!! Processing for a wavelength of " + str(w*1e9) + "nm...")   
     
     if cfg_sim['ADD_WFE']:
       # find appropriate zemax wfe file
@@ -228,7 +238,7 @@ if __name__== "__main__":
     s.datacube.addComposite(res)
     
   if args.f:
-    s.datacube.write(args.fn, cfg['RESAMPLE'], cfg['SAMPLING_FACTOR'], cfg['FOV'])
+    s.datacube.write(args.fn, resampling_im, cfg['RESAMPLING_FACTOR'], cfg['HFOV'], verbose=True)
     if args.fv:
       d = pyds9.DS9()
       d.set("file composite.fits")